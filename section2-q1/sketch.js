e4005d4a46db80f0d3b79627bbb24c6b1f74d21a
mihofunaoka <114969896+mihofunaoka@users.noreply.github.com>
2022-10-15 18:05:35 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,19 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
-}
+function setup() {
+  createCanvas(100, 100);
+  
+  for(let i=0; i<10; i++){
+    noFill();
+    let x=i*10+10
+    
+    if(i<5){
+      stroke(0,0,255);
+  
+    }
+    else{
+      stroke(255,0,0);
+      
+    }
+    ellipse(50,50,x);
+    }
+      
